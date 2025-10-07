fdb604e807baef310b4e3923082ea4e7b63cc292
Hossein Ameri <ameri.h22@gmail.com>
2025-10-07 21:12:24 +0330

    empting the one class

--- one.java
+++ one.java
@@ -3,12 +3,6 @@ public class one{
 public static void main(String[] args){
 
 
-	System.out.println(" Hossei Ameri");
-
-	System.out.println("Hello world");
-
-	//System.out.println("Hello ali");
-
 
 
 }
